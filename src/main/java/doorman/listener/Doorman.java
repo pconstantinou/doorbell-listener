d8299e8069fb48a34789ecb48d7d75f765f04063
Philip Constantinou <constantinou@gmail.com>
2017-12-15 14:36:13 -0800

    Initial commit

---
+++ src/main/java/doorman/listener/Doorman.java
@@ -0,0 +1,60 @@
+package doorman.listener;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.pusher.client.Pusher;
+import com.pusher.client.PusherOptions;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.SubscriptionEventListener;
+
+public class Doorman {
+
+	public static void main(String[] args) throws Exception {
+
+		Properties props = new Properties();
+		props.load(new FileInputStream(args[1]));
+
+		String appId = props.getProperty("app_id");
+		if (appId == null) {
+			System.err.println("No secret provided");
+		} else {
+			System.out.println("Using App Id: " + appId);
+		}
+		String key = props.getProperty("app_key");
+		String secret = props.getProperty("secret");
+		if (secret == null) {
+			System.err.println("No secret provided");
+		}
+		String cluster = props.getProperty("cluster", "us2");
+		String channelName = props.getProperty("channel", "my-channel");
+		String eventName = props.getProperty("event", "my-event");
+		final String command = props.getProperty("command");
+
+		PusherOptions options = new PusherOptions();
+		options.setCluster(cluster);
+		Pusher pusher = new Pusher(key, options);
+		Channel channel = pusher.subscribe(channelName);
+
+		channel.bind(eventName, new SubscriptionEventListener() {
+			public void onEvent(String channelName, String eventName, final String data) {
+				System.out.println(data);
+				try {
+					System.out.println("Executing:" + command);
+					Runtime.getRuntime().exec(command);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		pusher.connect();
+
+		while (true) {
+			Thread.sleep(1000);
+			System.out.println("sleeping");
+
+		}
+	}
+}

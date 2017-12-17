79f528afc93fd79558bbda511afb09f7181190f1
Philip Constantinou <constantinou@gmail.com>
2017-12-16 18:02:00 -0800

    Added support for a password/properties file

--- src/main/java/doorman/listener/Doorman.java
+++ src/main/java/doorman/listener/Doorman.java
@@ -1,10 +1,14 @@
 package doorman.listener;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
 import java.io.File;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.pusher.client.Pusher;
 import com.pusher.client.PusherOptions;
 import com.pusher.client.channel.Channel;
@@ -17,12 +21,22 @@ public class Doorman {
 		File propFile = new File(args[0]);
 		System.out.println("Using " + propFile);
 		if (!propFile.exists()) {
-		    System.out.println("Properties don't exist");
+			System.out.println("Properties don't exist");
+			System.exit(-1);
 		} else {
-		    props.load(new FileInputStream(propFile));
-		    props.list(System.out);
+			props.load(new FileInputStream(propFile));
+			props.list(System.out);
 		}
-	    
+
+		final Properties passwords = new Properties();
+		final File passwordsFile = new File(args[1]);
+		if (!passwordsFile.isFile() && passwordsFile.canRead()) {
+			System.err.println("Bad password file: " + passwordsFile);
+			System.exit(-1);
+		}
+
+		passwords.load(new FileInputStream(passwordsFile));
+
 		String appId = props.getProperty("app_id");
 		if (appId == null) {
 			System.err.println("No secret provided");
@@ -46,12 +60,23 @@ public class Doorman {
 
 		channel.bind(eventName, new SubscriptionEventListener() {
 			public void onEvent(String channelName, String eventName, final String data) {
-				System.out.println(data);
 				try {
-					System.out.println("Executing:" + command);
-					Runtime.getRuntime().exec(command);
-				} catch (IOException e) {
-					e.printStackTrace();
+					passwords.load(new FileInputStream(passwordsFile));
+				} catch (IOException e1) {
+					System.err.println("Can't reload the password file:" + passwordsFile);
+				}
+				System.out.println("Data:" + data);
+				JsonElement element = new JsonParser().parse(data);
+				String message = element.getAsJsonObject().get("message").getAsString();
+
+				String user = passwords.getProperty(message);
+				if (user != null) {
+					try {
+						System.out.println(new Date() + ": Executing:" + command + " for " + user);
+						Runtime.getRuntime().exec(command);
+					} catch (Throwable e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		});
